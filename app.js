d6f45409012b51647ef71823479689a417670b20
SANJAY <sanjaykumarskr2025@gmail.com>
2026-09-01 21:16:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const RailOptix = {
+version: "1.0.0",
+stations: [],
+trains: [],
+maintenanceBlocks: [],
 
+};
+
+document.addEventListener(
+"DOMContentLoaded",
+function() {
+RailOptix.init();
+}
+);
+
+window.RailOptix = RailOptix;
+
+END
